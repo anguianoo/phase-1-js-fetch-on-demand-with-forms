cc39c19ee745a028cc1956b3bf168f4ff36e63c7
anguianoo <njanguiano@gmail.com>
2022-05-06 20:59:06 -0700

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
-}
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let userInput = e.target.children[1].value;
+    const input = document.querySelector("input#searchByID");
+    fetchData(userInput);
+  });
+  const fetchData = (input) => {
+    fetch(`http://localhost:3000/movies/${input}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
 
-document.addEventListener('DOMContentLoaded', init);
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  };
+};
+
+document.addEventListener("DOMContentLoaded", init);
